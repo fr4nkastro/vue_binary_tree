7c089992a4ea0bb3ea1853dca63e6fd4c5984e91
fr4nkastro <85426216+fr4nkastro@users.noreply.github.com>
2022-08-11 23:34:25 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vue_binary_tree',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
